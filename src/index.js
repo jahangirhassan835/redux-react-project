8fb541d6c47cccd5380d232acfb8baca440a954a
Jahangir Hassan <835-2019@hamdard.edu>
2023-07-29 12:13:18 +0500

    add store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import {legacy_createStore as createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from'./Services/Reducter/index'
 
